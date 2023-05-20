358d03ab1d4eb3d9cf6e35b72b5d633d9d332f31
wizmonkey <wizmonkey1337@gmail.com>
2023-05-20 12:41:17 +0530

    +misc

--- app/screens/Tester.js
+++ app/screens/Tester.js
@@ -1,123 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import * as Location from 'expo-location';
-import { getDatabase, ref, onValue, off } from 'firebase/database';
+import React, { useContext } from "react";
+import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 
-const SearchAndMapScreen = () => {
-  const [busStops, setBusStops] = useState([]);
-  const [selectedBusStop, setSelectedBusStop] = useState(null);
-  const [userLocation, setUserLocation] = useState(null);
+import themeContext from "../config/themeContext";
 
-  useEffect(() => {
+function Route() {
+  const theme = useContext(themeContext);
+  const styles = getStyles(theme);
 
-
-    // Fetch bus stops from Firebase Realtime Database
-    const db = getDatabase();
-    const busStopsRef = ref(db, 'busStops');
-    onValue(busStopsRef, (snapshot) => {
-      const data = snapshot.val();
-      if (data) {
-        const busStopsArray = Object.keys(data).map((key) => ({
-          id: key,
-          ...data[key],
-        }));
-        setBusStops(busStopsArray);
-      }
-    });
-
-    // Request user's location using Expo's Location service
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        console.log('Permission to access location was denied');
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setUserLocation(location.coords);
-    })();
-
-    return () => {
-      // Clean up listeners or resources if needed
-      off(busStopsRef);
-    };
-  }, []);
-
-  const handleBusStopSelect = (busStop) => {
-    setSelectedBusStop(busStop);
-  };
-
-  const renderBusStopItem = ({ item }) => {
-    return (
-      <TouchableOpacity onPress={() => handleBusStopSelect(item)} style={styles.busStopItem}>
-        <Text style={styles.busStopName}>{item.stopName}</Text>
-      </TouchableOpacity>
-    );
-  };
-  
-  return (
+    return ( 
     <View style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={busStops}
-          keyExtractor={(item) => item.id}
-          renderItem={renderBusStopItem}
-        />
-      </View>
-      <View style={styles.mapContainer}>
-        {userLocation && (
-          <MapView
-            style={styles.map}
-            initialRegion={{
-              latitude: userLocation.latitude,
-              longitude: userLocation.longitude,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421,
-            }}
-          >
-            {selectedBusStop && (
-              <Marker
-                coordinate={{
-                  latitude: selectedBusStop.latitude,
-                  longitude: selectedBusStop.longitude,
-                }}
-              />
-            )}
-          </MapView>
-        )}
-      </View>
-    </View>
+    <TextInput
+        style={styles.input}
+        label="Input"
+        placeholder="Enter Details"
+      />
+    <View>
+        <Button title="Submit"> Get Details</Button>
+  </View>
+</View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white', // Add this line
-  },
-  listContainer: {
-    flex: 1,
-    backgroundColor: 'white', // Add this line
-  },
-  mapContainer: {
-    flex: 2,
-    backgroundColor: 'white', // Add this line
-  },
-  map: {
-    flex: 1,
-  },
-  busStopItem: {
-    paddingVertical: 16,
-    paddingHorizontal: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: 'gray',
-  },
-  busStopName: {
-    fontSize: 16,
-    color: 'black',
-  },
-});
+const getStyles = (theme) => 
+  StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.background,
+        color: theme.color,
+        justifyContent: 'center',
+    },
+    topText: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginVertical: 15,
+        color: theme.color,
+        marginHorizontal: '5%',
+    },
+    text: {
+        fontSize: 16,
+        margin: 10,
+        color: theme.color,
+    },
+    input:  {
+      borderWidth: 1,
+      borderRadius: 10,
+      padding: 10,
+      marginBottom: 10
+    }
+})
 
-
-export default SearchAndMapScreen;
+export default Route; 
